2c6df20c64af8d72494b1e5b36a5066a417706e0
Diego Cáceres Cardoza <diegokaiser@gmail.com>
2024-10-22 21:09:47 +0200

    nodemon

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import { router } from './routes/printers.js'
 import { corsMiddleware }  from './middlewares/cors.js'
 
 const app = express()
-app.use(json())
+app.use(express.json())
 app.use(corsMiddleware())
 app.disable('x-powered-by')
 
